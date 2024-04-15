e621e98157a82a49aca24a12b1b6dc0d9cbca4e1
Bijender Kumar <91715718+Bijender1@users.noreply.github.com>
2024-04-16 02:01:33 +0530

    Update helper.py
    Resolve the issue of emojis

--- helper.py
+++ helper.py
@@ -80,12 +80,84 @@ def most_common_words(selected_user,df):
     most_common_df = pd.DataFrame(Counter(words).most_common(20))
     return most_common_df
 
-def emoji_helper(selected_user,df):
+# def emoji_helper(selected_user, df):
+#     if selected_user != 'Overall':
+#         df = df[df['user'] == selected_user]
+#
+#     emoji_counts = {}
+#     for message in df['message']:
+#         emojis_in_message = [c for c in message if c in emoji.UNICODE_EMOJI['en']]
+#         for emoji_char in emojis_in_message:
+#             if emoji_char in emoji_counts:
+#                 emoji_counts[emoji_char] += 1
+#             else:
+#                 emoji_counts[emoji_char] = 1
+#
+#     emoji_df = pd.DataFrame(list(emoji_counts.items()), columns=['Emoji', 'Count'])
+#     emoji_df = emoji_df.sort_values(by='Count', ascending=False)
+#
+#     return emoji_df
+
+def is_emoji(char):
+        emoji_ranges = [
+            (0x1F300, 0x1F320), (0x1F330, 0x1F335), (0x1F337, 0x1F37C),
+            (0x1F380, 0x1F393), (0x1F3A0, 0x1F3C4), (0x1F3C6, 0x1F3CA),
+            (0x1F3E0, 0x1F3F0), (0x1F400, 0x1F43E), (0x1F440, 0x1F440),
+            (0x1F442, 0x1F4F7), (0x1F4F9, 0x1F4FC), (0x1F500, 0x1F53C),
+            (0x1F540, 0x1F543), (0x1F550, 0x1F567), (0x1F5FB, 0x1F5FF),
+            (0x1F600, 0x1F64F), (0x1F680, 0x1F6C5), (0x1F6CC, 0x1F6CC),
+            (0x1F90D, 0x1F90D), (0x1F90F, 0x1F90F), (0x1F911, 0x1F917),
+            (0x1F920, 0x1F927), (0x1F930, 0x1F930), (0x1F933, 0x1F93E),
+            (0x1F940, 0x1F94B), (0x1F950, 0x1F95E), (0x1F980, 0x1F991),
+            (0x1F9C0, 0x1F9C0), (0x1F9D0, 0x1F9E6), (0x200D, 0x200D),
+            (0x203C, 0x203C), (0x2049, 0x2049), (0x2122, 0x2122),
+            (0x2139, 0x2139), (0x2194, 0x2199), (0x21A9, 0x21AA),
+            (0x231A, 0x231B), (0x2328, 0x2328), (0x23CF, 0x23CF),
+            (0x23E9, 0x23F3), (0x23F8, 0x23FA), (0x24C2, 0x24C2),
+            (0x25AA, 0x25AB), (0x25B6, 0x25B6), (0x25C0, 0x25C0),
+            (0x25FB, 0x25FE), (0x2600, 0x2604), (0x260E, 0x260E),
+            (0x2611, 0x2611), (0x2614, 0x2615), (0x2618, 0x2618),
+            (0x261D, 0x261D), (0x2620, 0x2620), (0x2622, 0x2623),
+            (0x2626, 0x2626), (0x262A, 0x262A), (0x262E, 0x262F),
+            (0x2638, 0x263A), (0x2640, 0x2640), (0x2642, 0x2642),
+            (0x2648, 0x2653), (0x2660, 0x2660), (0x2663, 0x2663),
+            (0x2665, 0x2666), (0x2668, 0x2668), (0x267B, 0x267B),
+            (0x267F, 0x267F), (0x2692, 0x2697), (0x2699, 0x2699),
+            (0x269B, 0x269C), (0x26A0, 0x26A1), (0x26AA, 0x26AB),
+            (0x26B0, 0x26B1), (0x26BD, 0x26BE), (0x26C4, 0x26C5),
+            (0x26C8, 0x26C8), (0x26CE, 0x26CF), (0x26D1, 0x26D1),
+            (0x26D3, 0x26D4), (0x26E9, 0x26EA), (0x26F0, 0x26F5),
+            (0x26F7, 0x26FA), (0x26FD, 0x26FD), (0x2702, 0x2702),
+            (0x2705, 0x2705), (0x2708, 0x2709), (0x270A, 0x270B),
+            (0x270C, 0x270D), (0x270F, 0x270F), (0x2712, 0x2712),
+            (0x2714, 0x2714), (0x2716, 0x2716), (0x271D, 0x271D),
+            (0x2721, 0x2721), (0x2728, 0x2728), (0x2733, 0x2734),
+            (0x2744, 0x2744), (0x2747, 0x2747), (0x274C, 0x274C),
+            (0x274E, 0x274E), (0x2753, 0x2755), (0x2757, 0x2757),
+            (0x2763, 0x2764), (0x2795, 0x2797), (0x27A1, 0x27A1),
+            (0x27B0, 0x27B0), (0x27BF, 0x27BF)]
+        return any(start <= ord(char) <= end for start, end in emoji_ranges)
+
+
+def emoji_helper(selected_user, df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
-    emoji_df = pd.DataFrame(Counter(emoji).most_common(len(Counter(emoji))))
 
-    return emoji_df
+    emoji_counts = {}
+    for message in df['message']:
+        emojis_in_message = [c for c in message if is_emoji(c)]
+        for emoji_char in emojis_in_message:
+            if emoji_char in emoji_counts:
+                emoji_counts[emoji_char] += 1
+            else:
+                emoji_counts[emoji_char] = 1
+
+    emoji_df = pd.DataFrame(list(emoji_counts.items()), columns=['Emoji', 'Count'])
+    emoji_df = emoji_df.sort_values(by='Count', ascending=False)
+
+    emoji_text = ''.join([emoji.emojize(f':{em}:') * count for em, count in emoji_counts.items()])  # Changed 'emoji' to 'em'
+
+    return emoji_df, emoji_text
 
 def monthly_timeline(selected_user,df):
 
